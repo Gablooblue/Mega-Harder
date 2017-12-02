e65e0999bda906ec0a15186bd5d636da1a0d4502
gablooblue <gabrielhenry.lopez@gmail.com>
2017-12-03 01:04:04 +0800

    Fixed routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
-  resources :prescriptions do
-      resources :prescription_medicines
-  end
   devise_for :users
   resources :medicine_types
   devise_scope :user do
     root :to => 'devise/sessions#new'
+  end
+
+  resources :users do
+    get 'prescriptions'
+    get 'transactions'
+    resources :prescriptions do
+	resources :prescription_medicines
+    end
   end
 end
