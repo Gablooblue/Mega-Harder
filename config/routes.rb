0348efb68aaef656a0f8b592d244b9f565640664
gablooblue <gabrielhenry.lopez@gmail.com>
2017-12-03 01:25:41 +0800

    Fixed routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'hello_world', to: 'hello_world#index'
   devise_for :users
   resources :medicine_types
   devise_scope :user do
@@ -7,9 +6,9 @@ Rails.application.routes.draw do
   end
 
   resources :users do
-    get 'transactions'
     resources :prescriptions do
 	resources :prescription_medicines
     end
+    resources :transactions
   end
 end
