d02bd65165f934c45d0dae04c3442993ff169366
L <gionellef@gmail.com>
2017-12-02 21:09:57 +0800

    Added transactions table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+MedicineType.delete_all
+MedicineType.create! id: 1, generic_name: "Paracetamol", description: "Anti headache", average_price: 16
+MedicineType.create! id: 2, generic_name: "Amoxicillin", description: "Anti biotic", average_price: 24
+MedicineType.create! id: 3, generic_name: "Penicillin", description: "Better Antibiotic", average_price: 32
+
+TransactionStatus.delete_all
+TransactionStatus.create! id: 1, name: "In Progress"
+TransactionStatus.create! id: 2, name: "New"
+TransactionStatus.create! id: 3, name: "Success"
+TransactionStatus.create! id: 4, name: "Failed"
